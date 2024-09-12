e425aa8cef604d068ac90c4eae0bfc588d0512b7
Perchik <saikalrustamovna@gmail.com>
2024-09-12 14:39:36 +0600

    init

---
+++ src/model/ComplexNum.java
@@ -0,0 +1,24 @@
+package model;
+
+public class ComplexNum {
+    private double real;
+    private double imaginary;
+
+    public ComplexNum(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    @Override
+    public String toString() {
+        return real + " + " + imaginary + "i";
+    }
+}
